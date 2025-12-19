81afbad5454837e822fce609354a425ec65cb352
SUHASH VELU <suhashvelu@gmail.com>
2025-12-19 10:59:50 +0530

    feat: enable JavaScript support in TypeScript and add type declarations for JSX modules and React components

---
+++ src/declarations.d.ts
@@ -0,0 +1,44 @@
+declare module '*.jsx' {
+    const value: any;
+    export default value;
+}
+
+declare module './components/Navbar' {
+    const Navbar: React.FC;
+    export default Navbar;
+}
+
+declare module './components/Hero' {
+    const Hero: React.FC;
+    export default Hero;
+}
+
+declare module './components/TechStack' {
+    const TechStack: React.FC;
+    export default TechStack;
+}
+
+declare module './components/Projects' {
+    const Projects: React.FC;
+    export default Projects;
+}
+
+declare module './components/Profiles' {
+    const Profiles: React.FC<any>; // Accepting any props for now due to recent changes
+    export default Profiles;
+}
+
+declare module './components/Contact' {
+    const Contact: React.FC;
+    export default Contact;
+}
+
+declare module './components/Footer' {
+    const Footer: React.FC;
+    export default Footer;
+}
+
+declare module './components/Loader' {
+    const Loader: React.FC;
+    export default Loader;
+}
